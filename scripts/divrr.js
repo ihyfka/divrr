9097afd7a50875cedcb8a49bbd09050f023a53fb
Obinna Moses <obinnamosco6@gmail.com>
2025-09-23 11:17:33 +0100

    Update divrr.js

--- scripts/divrr.js
+++ scripts/divrr.js
@@ -57,7 +57,7 @@ const activeGoForwards = document.querySelector(".active-forwards-svg");
 for(l=0;l<needPremiumService.length;l++){
   needPremiumService[l].addEventListener("click", ()=>{
     setTimeout(()=>{
-      window.open("../markupL/getpremium.html"); 
+      window.open("/markupL/getpremium.html"); 
     }, 300)
   })
 }
@@ -445,3 +445,4 @@ setInterval(()=>{
   alert("Local Cache is being cleared frequently to aid device resource allocation optimization :)")
   localStorage.clear();
 }, 7200000)
+
