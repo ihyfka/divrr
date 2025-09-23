1692d4105a9b7f988a63b718933dc18e188d5951
Obinna Moses <obinnamosco6@gmail.com>
2025-09-23 12:14:41 +0100

    Update checkout.js
    user/org to project

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,7 +5,7 @@ const btn = document.querySelector("button");
 //going back
 back.addEventListener("click", ()=>{
   setTimeout(()=>{
-    location.href = "/markupl/getpremium.html";
+    location.href = "markupl/getpremium.html";
   }, 300)
 })
 
@@ -14,6 +14,7 @@ back.addEventListener("click", ()=>{
 //proceeding
 btn.addEventListener("click", ()=>{
   setTimeout(()=>{
-    location.href = "/markupl/makepayment.html";
+    location.href = "markupl/makepayment.html";
   }, 300)
+
 })
